c842897183591425c86d25da8f7a3ea1c0b4c174
Neelisha88 <nilisharawat88@gmail.com>
2025-06-17 17:05:05 +0530

    [Nilisha] Add. Basic calculator added

---
+++ Day1_ProgrammingElements/BasicCalci.java
@@ -0,0 +1,14 @@
+import java. util.*;
+public class BasicCalci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the Second number : ");
+        int b = sc.nextInt();
+        System.out.println(" The addition, subtraction, multiplication and division value of 2 numbers " + a + 
+                " and " + b + " is " + (a+b) + " , " + (a-b) + " , " + (a *b) + " , " + (a /b)
+                 );
+        sc.close();
+    }
+}              
