8718f54e5ed6365fd5709a4e35e64915797dd4dc
Neelisha88 <nilisharawat88@gmail.com>
2025-06-19 13:30:12 +0530

    [Nilisha] Add. Sum until Zero or negative

---
+++ Day2_ControlFlow/SumUntilZeroOrNegative.java
@@ -0,0 +1,19 @@
+package Day2_ControlFlow;
+import java.util.*;
+
+public class SumUntilZeroOrNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double total = 0.0; 
+        while (true) {
+            System.out.print("Enter a number (0 or negative to stop): ");
+            double input = sc.nextDouble();
+            if (input <= 0) {
+                break; 
+            }
+            total += input;
+        }
+        System.out.println("The total sum is: " + total);
+        sc.close();
+    }
+}
