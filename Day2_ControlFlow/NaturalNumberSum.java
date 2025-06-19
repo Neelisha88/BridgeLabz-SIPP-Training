5cd8f92299573a463703d656936f8c0080e73f0d
Neelisha88 <nilisharawat88@gmail.com>
2025-06-19 10:57:35 +0530

    [Nilisha]Add. Natural numbers sum

---
+++ Day2_ControlFlow/NaturalNumberSum.java
@@ -0,0 +1,21 @@
+package Day2_ControlFlow;
+import java.util.Scanner;
+
+public class NaturalNumberSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        System.out.print("Enter the Number : ");
+        int num = sc.nextInt();
+        if( num >= 1)
+        {
+            sum = num * (num + 1) / 2;
+            System.out.println("The sum of " + num + " natural numbers is " + sum);
+        }
+        else{
+            System.out.println("The number " + num + " is not a natural number");
+        }
+        sc.close();
+    }
+    
+}
