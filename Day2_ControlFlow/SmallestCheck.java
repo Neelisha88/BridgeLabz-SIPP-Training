8163731350a16df6e709698da6ef51fa8db749b8
Neelisha88 <nilisharawat88@gmail.com>
2025-06-19 12:31:52 +0530

    [Nilisha]Add. Smallest check

---
+++ Day2_ControlFlow/SmallestCheck.java
@@ -0,0 +1,18 @@
+package Day2_ControlFlow;
+import java.util.*;
+
+public class SmallestCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number : ");
+        int b = sc.nextInt();
+        System.out.print("Enter Third Number : ");
+        int c = sc.nextInt();
+        boolean isSmallest = a < b && a < c ;
+        System.out.println("Is the first number the smallest? " + isSmallest);
+        sc.close();
+    }
+    
+}
